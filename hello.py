828a39804c6e140c666523feb280c3b1c61d3020
“lioamingsi@gmail.com” <mingsi283>
2023-02-17 22:29:31 +0800

    第一次修改

---
+++ hello.py
@@ -0,0 +1,11 @@
+'''
+Description: 
+Author: mingsi
+Date: 2022-07-13 19:53:12
+LastEditTime: 2022-07-13 19:53:18
+LastEditors: mingsi
+FilePath: /Python爬虫/hello.py
+联系:liaomingsi@gmail.com
+'''
+input("hello")
+print("Hello")
